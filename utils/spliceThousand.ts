6f1689e56b7c95e3e07bed8c2d2f854e532b5903
JiquanWang99 <951873902@qq.com>
2021-08-28 23:13:08 +0800

    feat: 增加千分位方法

---
+++ utils/spliceThousand.ts
@@ -0,0 +1,15 @@
+/**
+ * 千分位转换
+ */
+function spliceThousand(number: string | number) {
+  let str = ''
+  const arr = number.toString().split('')
+  let length = arr.length
+
+  while (length > 3) {
+    str = `,${arr.splice(-3).join('')}` + str
+    length = arr.length
+  }
+
+  return arr.join('') + str
+}
